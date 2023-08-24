d87bf0a5d2a8fcb1e249cde79b6ac3ffc8b3d7a5
mathewkoech <mathewkoech55@gmail.com>
2023-08-24 18:20:08 +0300

    updated models

---
+++ mealapp/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.urls import reverse
+
+class Meal(models.Model):
+    name  = models.CharField(max_length = 200)
+    ingredients = models.CharField(max_length = 500, default=None)
+    price = models.FloatField(null=True, blank=True)
+    image_url = models.CharField(max_length = 2083, default=False)
+    meal_available = models.BooleanField(default=False)
+
+    def __str__(self):
+	    return self.name
+
+
+class Order(models.Model):
+	product = models.ForeignKey(Meal, max_length=200, null=True, blank=True, on_delete = models.SET_NULL)
+	created =  models.DateTimeField(auto_now_add=True) 
+
+	def __str__(self):
+		return self.product.name
---
+++ mealapp/views.py
@@ -0,0 +1,44 @@
+# from django.shortcuts import render 
+# from django.views.generic import ListView, DetailView
+# from django.views.generic.edit import CreateView
+# from .models import Meal, Order
+# from django.urls import reverse_lazy
+# from django.db.models import Q # for search method
+# from django.http import JsonResponse
+# import json
+
+
+
+# class BooksListView(ListView):
+#     model = Meal
+#     template_name = 'list.html'
+
+
+# class BooksDetailView(DetailView):
+#     model = Meal
+#     template_name = 'detail.html'
+
+
+# class SearchResultsListView(ListView):
+# 	model = Meal
+# 	template_name = 'search_results.html'
+
+# 	def get_queryset(self): # new
+# 		query = self.request.GET.get('q')
+# 		return Meal.objects.filter(
+# 		Q(title__icontains=query) | Q(author__icontains=query)
+# 		)
+
+# class BookCheckoutView(DetailView):
+#     model = Meal
+#     template_name = 'checkout.html'
+
+
+# def paymentComplete(request):
+# 	body = json.loads(request.body)
+# 	print('BODY:', body)
+# 	product = Meal.objects.get(id=body['productId'])
+# 	Order.objects.create(
+# 		product=product
+# 	)
+# 	return JsonResponse('Payment completed!', safe=False)
